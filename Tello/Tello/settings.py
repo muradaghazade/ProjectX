2c3bac68f2ed040aa0cc3a605b82bb01051c655c
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2022-01-30 20:57:24 +0400

    dockerization

--- Tello/Tello/settings.py
+++ Tello/Tello/settings.py
@@ -88,11 +88,19 @@ WSGI_APPLICATION = 'Tello.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'taskilled'),
+            'USER': os.getenv('POSTGRES_USER', 'taskilled'),
+            'PORT': os.getenv('POSTGRES_PORT', 5432),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345'),
+            'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
+        }
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     }
-}
 
 
 # Password validation
